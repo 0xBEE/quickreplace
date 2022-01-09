2b3150594e87af3231a8e2a02b829b4115f19c2c
0xBEE <48181581+0xBEE@users.noreply.github.com>
2022-01-09 15:32:25 -0500

    Reformat

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+use regex::{Error, Regex};
+use std::{env, fs};
 use text_colorizer;
 use text_colorizer::Colorize;
-use std::{env, fs};
-use regex::{Error, Regex};
 
 #[derive(Debug)]
 struct Arguments {
     target: String,
     replacement: String,
     filename: String,
-    output: String
+    output: String,
 }
 
 fn print_usage() {
-    eprintln!("{} - Change occurrences of one string into another", "quickreplace".green());
+    eprintln!(
+        "{} - Change occurrences of one string into another",
+        "quickreplace".green()
+    );
     eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
 }
 
@@ -21,7 +24,11 @@ fn parse_args() -> Arguments {
 
     if args.len() != 4 {
         print_usage();
-        eprintln!("{} wrong number of arguments: expected 4, got {}", "Error:".red().bold(), args.len());
+        eprintln!(
+            "{} wrong number of arguments: expected 4, got {}",
+            "Error:".red().bold(),
+            args.len()
+        );
         std::process::exit(1);
     }
 
@@ -29,7 +36,7 @@ fn parse_args() -> Arguments {
         target: args[0].clone(),
         replacement: args[1].clone(),
         filename: args[2].clone(),
-        output: args[3].clone()
+        output: args[3].clone(),
     }
 }
 
@@ -43,7 +50,12 @@ fn main() {
     let data = match fs::read_to_string(&args.filename) {
         Ok(v) => v,
         Err(e) => {
-            eprintln!("{} failed to read from file '{}': {:?}", "Error:".red().bold(), args.filename, e);
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
             std::process::exit(1);
         }
     };
@@ -59,7 +71,12 @@ fn main() {
     match fs::write(&args.output, &replaced_data) {
         Ok(_) => (),
         Err(e) => {
-            eprintln!("{} failed to write to file '{}': {:?}", "Error:".red().bold(), args.filename, e);
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
             std::process::exit(1);
         }
     }
